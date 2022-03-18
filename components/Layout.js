396d1f7d4af21f2a21d7407b0d1bec40f061794d
JMEChristian Digital <jmechristian@gmail.com>
2022-03-18 08:37:35 -0400

    testimonial header

--- components/Layout.js
+++ components/Layout.js
@@ -11,34 +11,37 @@ const Layout = ({ children }) => {
         <meta name='description' content='Real estate, redefined' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <nav>
-        <nav className='wrapper'>
-          <div className='nav__inner'>
-            <div className='nav__logo'>
-              <Logo />
+      <nav className='wrapper'>
+        <div className='nav__inner'>
+          <div className='nav__logo'>
+            <Logo />
+          </div>
+          <div className='nav__menu'>
+            <div className='nav__item'>
+              <Link href='/'>1</Link>
             </div>
-            <div className='nav__menu'>
-              <div className='nav__item'>
-                <Link href='/'>1</Link>
-              </div>
-              <div className='nav__item'>
-                <Link href='/second'>2</Link>
-              </div>
-              <div className='nav__item'>
-                <Link href='/third'>3</Link>
-              </div>
-              <div className='nav__item'>
-                <Link href='/fourth'>4</Link>
-              </div>
-              <div className='nav__item'>
-                <Link href='/stars'>Stars</Link>
-              </div>
+            <div className='nav__item'>
+              <Link href='/second'>2</Link>
+            </div>
+            <div className='nav__item'>
+              <Link href='/third'>3</Link>
+            </div>
+            <div className='nav__item'>
+              <Link href='/fourth'>4</Link>
+            </div>
+            <div className='nav__item'>
+              <Link href='/stars'>Stars</Link>
             </div>
           </div>
-        </nav>
+        </div>
       </nav>
       <div className='main__wrapper'>
-        <div className='main__container'>{children}</div>
+        <div className='main__container'>
+          <div className='main__header'>
+            [Agent Name/ Team Name] Testimonials
+          </div>
+          {children}
+        </div>
       </div>
     </>
   );
